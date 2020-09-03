c1e7bffacb3840734f4d04f5697f89b1fc466521
Abhiram Menni <>
2020-09-03 16:34:11 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Spin from './spin/spin';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Spin />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/spin/spin.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+
+import './spin.scss'
+
+const Spin = () => {
+
+    return (
+        <div className="spin">
+            <div className="spin__container">
+            
+            </div>
+        </div>
+    )
+
+}
+
+export default Spin;
